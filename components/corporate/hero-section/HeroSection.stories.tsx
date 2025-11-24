eda8d4b0be48c9bc19d7f22879e68ec61e08f127
Velizar Mihaylov <veli.mihaylov@honu.ai>
2025-11-24 12:23:20 +0000

    Updating components for corporate template to use the correct version of storybook.

--- components/corporate/hero-section/HeroSection.stories.tsx
+++ components/corporate/hero-section/HeroSection.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Meta, StoryFn } from '@storybook/react';
+import { Meta, StoryFn } from '@storybook/nextjs';
 import HeroSection from './HeroSection';
 
 export default {
